1f0db8f9498611cdbe5c0d89f930dab1028bc3de
ibk01161 <rkxmsdk@naver.com>
2024-07-16 00:33:56 +0900

    Add global Exception handler

---
+++ spring-api-app/src/main/java/com/app/global/error/ErrorCode.java
@@ -0,0 +1,19 @@
+package com.app.global.error;
+
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+public class ErrorCode {
+
+    private HttpStatus httpStatus;
+    private String errorCode;
+    private String message;
+
+    // 생성자
+    ErrorCode(HttpStatus httpStatus, String errorCode, String message) {
+        this.httpStatus = httpStatus;
+        this.errorCode = errorCode;
+        this.message = message;
+    }
+}
